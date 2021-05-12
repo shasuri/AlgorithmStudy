4edd64eab634b57e0662e2495debb0ff72f8cfb6
shasuri <ghimmk@DESKTOP-8BJ2BRS.localdomain>
2021-05-12 21:17:26 +0900

    test

--- albbu/10th_week/2020_kakao_1.py
+++ albbu/10th_week/2020_kakao_1.py
@@ -1,22 +1,22 @@
 def solution(new_id):
     
-    new_id = new_id.lower()
+    new_id = new_id.lower() # step1
+    
     temp_id = ""
 
-    for strIndex in range(len(new_id)) :
-        
-        if not new_id[strIndex].isalnum() :
+    for strIndex in range(len(new_id)) :    
+        if not new_id[strIndex].isalnum() : # step2, 소문자 제외
             if new_id[strIndex] == '-' or new_id[strIndex] == '_' or new_id[strIndex] == '.' :
-                temp_id += new_id[strIndex]
+                temp_id += new_id[strIndex] # step2, -_. 제외
 
         else :
-            temp_id += new_id[strIndex]
+            temp_id += new_id[strIndex] # 제외된 문자 추가
         
     new_id = temp_id
     
     temp_id = ""
     dotCount = 0
-    for strIndex in range(len(new_id)) :
+    for strIndex in range(len(new_id)) : # step3, .줄이기
         
         if new_id[strIndex] == "." :
             if dotCount == 0 :
@@ -28,20 +28,20 @@ def solution(new_id):
             temp_id += new_id[strIndex]
             dotCount = 0
         
-    temp_id = temp_id.strip('.')
+    temp_id = temp_id.strip('.') # step4
 
     strSize = len(temp_id)
 
-    if strSize == 0 :
+    if strSize == 0 : # step5
         temp_id += "a"
         strSize += 1
 
-    if strSize > 15 :
+    if strSize > 15 : # step6
         temp_id = temp_id[:15]
         temp_id = temp_id.rstrip('.')
         strSize = 15
 
-    if strSize <= 2 :
+    if strSize <= 2 : # step7
         last_char = temp_id[-1]
         
         for strIndex in range(3 - strSize) :
