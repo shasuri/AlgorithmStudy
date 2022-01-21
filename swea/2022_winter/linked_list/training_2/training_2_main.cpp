bd09079fc1153909888d076401f08dae9a416713
shasuri <ghimmk@gmail.com>
2022-01-21 19:02:27 +0900

    Linked list training 2 done.

---
+++ swea/2022_winter/linked_list/training_2/training_2_main.cpp
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "training_2_solution.cpp"
+#define MAX_NODE 10000
+
+enum { ADD_HEAD = 1, ADD_TAIL, ADD_NUM, FIND, REMOVE, PRINT, PRINT_REVERSE, END = 99 };
+
+extern void init();
+extern void addNode2Head(int data);
+extern void addNode2Tail(int data);
+extern void addNode2Num(int data, int num);
+extern int findNode(int data);
+extern void removeNode(int data);
+extern int getList(int output[MAX_NODE]);
+extern int getReversedList(int output[MAX_NODE]);
+
+static void run() {
+	while (1) {
+		int cmd, data, num, cnt, i = 0;
+		int output[MAX_NODE] = { 0 };
+
+		scanf("%d", &cmd);
+		switch (cmd) {
+		case ADD_HEAD: // 1
+			scanf("%d", &data);
+			addNode2Head(data);
+			break;
+		case ADD_TAIL: // 2
+			scanf("%d", &data);
+			addNode2Tail(data);
+			break;
+		case ADD_NUM: // 3
+			scanf("%d %d", &data, &num);
+			addNode2Num(data, num);
+			break;
+		case FIND: // 4
+			scanf("%d", &data);
+			num = findNode(data);
+			printf("%d\n", num);
+			break;
+		case REMOVE: // 5
+			scanf("%d", &data);
+			removeNode(data);
+			break;
+		case PRINT: // 6
+			cnt = getList(output);
+			while (cnt--)
+				printf("%d ", output[i++]);
+			printf("\n");
+			break;
+		case PRINT_REVERSE: // 7
+			cnt = getReversedList(output);
+			while (cnt--)
+				printf("%d ", output[i++]);
+			printf("\n");
+			break;
+		case END:
+			return;
+		}
+	}
+}
+
+int main(void) {
+	setbuf(stdout, NULL);
+	freopen("dll_input.txt", "r", stdin);
+
+	int T;
+	scanf("%d", &T);
+	for (int t = 1; t <= T; t++) {
+		printf("#%d\n", t);
+		init();
+		run();
+		printf("\n");
+	}
+
+	return 0;
+}
